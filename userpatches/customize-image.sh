c5aa5a25bad1905d6fa2ff3c31a063153aef5bb1
Sage Liem <sage.liem.9@gmail.com>
2025-11-11 13:49:53 -0600

    update provisioning script

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -4,12 +4,15 @@ apt install dosfstools
 
 touch /root/resize_data_partition.sh
 cat << EOF > /root/provisioning.sh
-datadevice=findmnt / | awk -F" " '/\/dev\// {print $2"}
-echo "Expanding data partition ${datadevice}"
-echo ", +" | sfdisk -N 2 ${datadevice}p2
+storagedevice=findmnt / | awk -F" " '/\/dev\// {print $2"}
+umount /mnt/data
+echo "Expanding data partition ${storagedevice}"
+echo ", +" | sfdisk -N 2 ${storagedevice}p2
 echo "Resizing FAT32 filesystem"
-fatresize -s 100% ${datadevice}p2
+fatresize -s 100% ${storagedevice}p2
 echo "Done"
+echo "Shutting down in 5 seconds. Add media and configuration files to the data partition of the SD card."
+sleep 5
 EOF
 
 apt install cage lighttpd chromium
